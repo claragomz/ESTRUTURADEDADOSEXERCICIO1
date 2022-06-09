2aa05e28fe08d8bb71123e97c6c789b703999d43
Ana Clara <gomesclara99x@gmail.com>
2022-06-09 13:30:40 -0300

    first commit

---
+++ exercicio1vainaweb.js
@@ -0,0 +1,10 @@
+var nome = 'ana clara'
+var idade = 22
+var ano = 2022
+var soma = 2022+22
+var sub = 2022-22
+console.log( O valor de nome é + nome)
+console.log( O valor de idade é + idade)
+console.log( O ano é + ano)
+console.log( A soma é + soma)
+console.log( A subtração é + sub)
